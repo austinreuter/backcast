bb3f8374a4f7a9701c9f18d3478e9aa638e85ae8
Hack Reactor Student <student@hackreactor.com>
2017-09-06 19:40:00 -0700

    Work video player view

--- src/views/app.js
+++ src/views/app.js
@@ -8,14 +8,21 @@ var AppView = Backbone.View.extend({
     this.render();
     const list = new VideoListView({el: $('#list'), collection: this.videos});
     list.render();
-    const player = new VideoPlayerView({el: $('#player'), collection: this.videos});
-    player.render();
+    this.player = new VideoPlayerView({el: $('#player'), collection: this.videos});
+    this.player.render();
+
+    this.videos.on('select', this.videoChange, this);
   },
 
 
   render: function() {
     this.$el.html(this.template());
     return this;
+  },
+
+  videoChange: function(e) {
+    const cur = new Videos(e);
+    new VideoPlayerView({el: $('#player'), collection: cur}).render();
   },
 
   template: templateURL('src/templates/app.html')
