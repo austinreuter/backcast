58349d132c47a62c22730440408d6982484f9b71
Claire Johnson <c.eliseee@gmail.com>
2017-09-07 08:15:05 -0700

    Attempt at search bar change

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -31,6 +31,12 @@ var Videos = Backbone.Collection.extend({
   },
 
   renderAll: function(array) {
+    // this.newVideos = new Videos(array);
+    // new VideoListView({el: $('#list'), collection: this.newVideos}).render();
+    // new VideoPlayerView({el: $('#player'), collection: this.newVideos}).render();
+    // // newVideoCollection.on('select', this.videoChange, this);
+    // this.newVideos.on('select', this.videoChange, this);
+
     window.exampleVideoData = array;
     new AppView();
   }
