2b009451b0d5970172cef70fc52aa63413a992e1
Hack Reactor Student <student@hackreactor.com>
2017-09-06 19:07:28 -0700

    Video Player tests passing

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,10 +1,12 @@
 var VideoPlayerView = Backbone.View.extend({
   initialize: function() {
+    this.model = this.collection.models[0];
     this.collection.on('sync', this.render, this);
+    this.collection.on('select', this.render, this);
   },
 
   render: function() {
-    this.$el.html('<div class="loading">Please wait...</div>');
+    this.$el.html(this.template(this.model.attributes));
     return this;
   },
 
