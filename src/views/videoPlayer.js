cd8fce3f195cc171b69429dc74b06b612fea4481
Hack Reactor Student <student@hackreactor.com>
2017-09-07 11:56:19 -0700

    Passing tests, added select call

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -10,6 +10,7 @@ var VideoPlayerView = Backbone.View.extend({
       this.$el.html('<div class="loading">Please wait...</div>');
     } else {
       this.$el.html(this.template(this.model.attributes));
+      this.model.select.call(this);
     }
     return this;
   },
